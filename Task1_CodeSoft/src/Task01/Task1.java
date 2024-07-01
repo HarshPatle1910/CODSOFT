001223be98a3d7c327e9acef2e65e3f29d0b3e0a
Harsh <Harsh@HarshP>
2024-07-02 00:50:50 +0530

    Added code to repeat this code as if wanna to play again this game

--- Task1_CodeSoft/src/Task01/Task1.java
+++ Task1_CodeSoft/src/Task01/Task1.java
@@ -13,36 +13,46 @@ public class Task1 {
         int guessed_number;
         boolean win = false;
         int attempt = 0;
+        boolean play_again=true;
         
         
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("Generated numbers are within "+min_range+" to "+max_range);
-        
-        
-        //Generated Random Number
-        generated_number = rand.nextInt(max_range - min_range + 1) + min_range;
-        System.out.println(generated_number);
-        
+       
         
         do {
-        	System.out.println("\n\nThis is your "+ ++attempt + " : ");
-            //Getting user input to guess for the generated number
-            System.out.print("\nEnter a number to guess for the generated number: ");
-            guessed_number = scan_obj.nextInt();
-            //System.out.println(guessed_number);
-    		
-            if (guessed_number<generated_number) {
-            	System.out.println("Guessed number " + guessed_number + " is too low.");
+        	 //Generated Random Number
+            generated_number = rand.nextInt(max_range - min_range + 1) + min_range;
+            System.out.println("\nYour generated number : "+generated_number);
+            
+        	while(!win) {
+            	System.out.println("\n\nThis is your "+ ++attempt + " attempt: ");
+                //Getting user input to guess for the generated number
+                System.out.print("\nEnter a number to guess for the generated number: ");
+                guessed_number = scan_obj.nextInt();
+                //System.out.println(guessed_number);
+        		
+                if (guessed_number<generated_number) {
+                	System.out.println("Guessed number " + guessed_number + " is too low.");
+                }
+                else if (guessed_number>generated_number) {
+                	System.out.println("Guessed number " + guessed_number + " is too high.");
+                } 
+                else {
+                	System.out.println("Guessed number " + guessed_number + " is correct.");
+                	System.out.println("Congratulations! You win the game.");
+                	win = true;
+                }
             }
-            else if (guessed_number>generated_number) {
-            	System.out.println("Guessed number " + guessed_number + " is too high.");
-            } 
-            else {
-            	System.out.println("Guessed number " + guessed_number + " is correct.");
-            	System.out.println("Congratulations! You win the game.");
-            	win = true;
-            }
-        }while(!win);
+            
+        	System.out.println("You took " + attempt + " attempts to guess the number");
+        	
+        	System.out.println("Do you want to play again(Yes or No)?");
+        	play_again = scan_obj.next().equalsIgnoreCase("Yes");
+        	win = false;
+        	attempt = 0;
+        }while (play_again);
         
+        System.out.println("Thanks to play game!");
 	}
 }
